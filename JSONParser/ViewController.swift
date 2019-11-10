04348019aea4df4e296777529d8882cc1a79a072
Andrii Halabuda <losangelesboy11@gmail.com>
2019-11-10 22:00:42 +0100

    JSON Serializatioin

--- JSONParser/ViewController.swift
+++ JSONParser/ViewController.swift
@@ -12,7 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos") else { return }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        guard let dataResponse = data, error == nil else {
+                  print(error?.localizedDescription ?? "Response Error")
+                  return }
+            do {
+                //here dataResponse received from a network request
+                let jsonResponse = try JSONSerialization.jsonObject(with:
+                                       dataResponse, options: [])
+//                print(jsonResponse)
+                
+                guard let jsonArray = jsonResponse as? [[String: Any]] else {
+                      return
+                }
+//                print(jsonArray)
+                
+                guard let title = jsonArray[0]["title"] as? String else { return }
+                print(title) 
+                
+             } catch let parsingError {
+                print("Error", parsingError)
+           }
+        }
+        task.resume()
+        
+
     }
 
 
